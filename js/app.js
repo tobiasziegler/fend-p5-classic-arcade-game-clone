5f89cf1e50437f3ce09a567e2bc590803271b1ed
David Mallard <correspondence@davidmallard.id.au>
2017-06-19 11:49:14 +1000

    Define all required functions and variables so app initialises without errors

--- js/app.js
+++ js/app.js
@@ -24,12 +24,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+};
+
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+
+};
+
+// Respond to a key press by the player
+Player.prototype.handleInput = function(key) {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
